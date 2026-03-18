c67c13cec20f5d8eab51d9bd65969f0d802b6490
Carlos Lopez <lopez.carlos.4807@eam.edu.co>
2026-03-18 09:45:40 -0500

    Se agrego la parte visula de registrarse

---
+++ app/src/main/java/com/qz/quantumfitzone/navegacion/NavGraph.kt
@@ -0,0 +1,35 @@
+package com.qz.quantumfitzone.navegacion
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.quantumfitzone.QuantumFitzoneScreen
+import com.qz.quantumfitzone.RegistroUsuarioForm
+
+@Composable
+fun NavGraph() {
+    val navController = rememberNavController()
+
+    NavHost(
+        navController = navController,
+        startDestination = Screen.Welcome.route
+    ) {
+        composable(Screen.Welcome.route) {
+            QuantumFitzoneScreen(
+                onRegistro = { navController.navigate(Screen.Registro.route) },
+                onLogin        = { navController.navigate(Screen.Login.route) },
+                onBack         = { navController.popBackStack() }
+            )
+        }
+
+        composable(Screen.Registro.route) {
+            RegistroUsuarioForm(
+                onBack = { navController.popBackStack() }
+            )
+        }
+
+        //composable(Screen.Login.route) {
+          //  LoginScreen(onBack = { navController.popBackStack() })
+        //}
+    }
+}
