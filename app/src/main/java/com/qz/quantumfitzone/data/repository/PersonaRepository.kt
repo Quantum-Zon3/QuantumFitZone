85fd85070d5cc300f591a3caacecec3459831696
Carlos Lopez <lopez.carlos.4807@eam.edu.co>
2026-04-10 10:35:30 -0500

    Se borro manejo de dependencia viejo

--- app/src/main/java/com/qz/quantumfitzone/data/repository/PersonaRepository.kt
+++
@@ -1,9 +0,0 @@
-package com.qz.quantumfitzone.data.repository
-
-import com.qz.quantumfitzone.data.model.PersonaEntity
-
-object PersonaRepository {
-    var listaPersonaEntities = mutableListOf(
-        PersonaEntity("Carlos","carlitos@gmail.com" ,"123",  "usuario", 70f, 1.75f, 0f, true),
-    )
-}
